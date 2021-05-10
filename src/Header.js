8b6db4ead7fb923463afeedca33d7aae4770aca4
vladimir <vladimirleonov789@gmail.com>
2021-05-10 19:56:52 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            Изучаем программирование
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+        </ul>
+    )
+}
+
+export default Technologies;
